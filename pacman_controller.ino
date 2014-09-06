788ab50a80b4bc9454393b7960a4570d1e0d09fe
Les Pounder <lespounder@gmail.com>
2014-09-06 18:48:43 +0100

    added comments to code

--- pacman_controller.ino
+++ pacman_controller.ino
@@ -1,33 +1,13 @@
-/* 
- Keyboard Button test
- 
- For the Arduino Leonardo and Micro.
- 
- Sends a text string when a button is pressed.
- 
- The circuit:
- * pushbutton attached from pin 2 to +5V
- * 10-kilohm resistor attached from pin 4 to ground
- 
- created 24 Oct 2011
- modified 27 Mar 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/KeyboardButton
- */
-
-const int UP = 4;          // input pin for pushbutton
-const int DOWN = 5;
-const int LEFT = 6;
-const int RIGHT = 7;
-const int FIRE = 8;
+const int UP = 4;          // input pin for up key
+const int DOWN = 5;        // input pin for down key
+const int LEFT = 6;        // input pin for left key
+const int RIGHT = 7;       // input pin for right key
+const int FIRE = 8;        // input pin for shooting
 //int previousButtonState = HIGH;   // for checking the state of a pushButton
 int counter = 0;                  // button push counter
 
 void setup() {
-  // make the pushButton pin an input:
+  // setup the pins as inputs:
   pinMode(UP, INPUT);
   pinMode(DOWN, INPUT);
   pinMode(LEFT, INPUT);
@@ -60,6 +40,8 @@ else if (digitalRead(RIGHT) == HIGH) {
 }  
 else if (digitalRead(FIRE) == HIGH) {
   Keyboard.press(KEY_LEFT_CTRL);
+  //Keyboard.press(KEY_LEFT_ALT);
+  //Keyboard.press(KEY_RIGHT_ARROW);
   delay(200);
   Keyboard.releaseAll();
 }  
